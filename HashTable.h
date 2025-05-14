f3dd92d711fe32d643485404928594df63952ba5
Marcelino Salazar <marcelinosalazar2@gmail.com>
2025-05-13 23:56:05 -0700

    changing the contents of the hashtable to display all clients and all information

--- HashTable.h
+++ HashTable.h
@@ -7,16 +7,16 @@ using namespace std;
 struct AllClientData;
 
 
-//creating a struct to hold each clients sub status and last day contacted in each bucket.
+/*creating a struct to hold each clients sub status and last day contacted in each bucket.
 struct ClientStatus {
     string subscription;
     int lastContacted;
-};
+};*/
 
 //here I create a struct for bucket to store the key, client status, and whether the slot is occupied or deleted.
 struct Bucket {
     int key;
-    ClientStatus value;
+    const AllClientData* value;
     bool isOccupied;
     bool isDeleted;
 
@@ -39,7 +39,7 @@ class HashTable {
     public:
         HashTable();
         ~HashTable();
-        bool insert(int key, const ClientStatus& value);
+        bool insert(int key, const AllClientData& value);
         void display() const;
         void unsubscribedClients(AllClientData* clientFile, int capacity);
         void searchPdays(int minDays);
--- hashtable.cpp
+++ hashtable.cpp
@@ -16,7 +16,7 @@ HashTable::~HashTable() {
 
 int collisionCount = 0;
 //here I create an insert function
-bool HashTable::insert(int key, const ClientStatus& value) {
+bool HashTable::insert(int key, const AllClientData* value) {
     int hashNum = hash(key);
 
 for (int i = 0; i < TABLE_SIZE; ++i) {
